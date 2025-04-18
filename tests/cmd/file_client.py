562e45d70404d070e62fd228ea8730d43c5dd01c
Hannes Weisbach <hannes.weisbach@gmail.com>
2025-04-18 09:10:24 +0200

    wip: cat test

--- tests/cmd/file_client.py
+++ tests/cmd/file_client.py
@@ -2,6 +2,7 @@ import contextlib
 from dataclasses import dataclass
 import pathlib
 import pytest
+import random
 import shutil
 import subprocess
 import tempfile
@@ -30,6 +31,15 @@ class RemoteFilePath:
     def __post_init__(self) -> None:
         # last time modifying the frozen dataclass
         object.__setattr__(self, "fs_path", self.fs_path / self.path[1:])
+
+
+class FileClient:
+    def __init__(self, node_id: int | str, env: dict[str, str]) -> None:
+        self._fs_node_id = f"{node_id}"
+        self._env = env
+
+    def __call__(self, cmd: str, *args: str, input: str | None = None) -> tuple[int, str, str]:
+        return execute_cli("fclnt", cmd, f"{self._fs_node_id}", *args, input=input, environment_variables=self._env)
 
 
 FileServerFactory = t.Callable[[int, TransportFactory], contextlib.AbstractContextManager[FileServerConfig]]
@@ -127,3 +137,28 @@ async def _unittest_mkdir(transport_factory: TransportFactory, fileserver_factor
 
         assert testdir.fs_path.is_dir()
 
+
+@pytest.mark.asyncio
+async def _unittest_cat(transport_factory: TransportFactory, fileserver_factory: FileServerFactory):
+    fs_node_id = 11
+    environment_variables = {
+        **transport_factory(100).environment,
+        "YAKUT_PATH": str(OUTPUT_DIR),
+    }
+    size = random.randint(10, 4096)
+    contents = random.randbytes(size).decode("utf-8", errors="ignore")
+    with fileserver_factory(fs_node_id, transport_factory) as fs:
+        fc = FileClient(fs_node_id, environment_variables)
+        testfile = RemoteFilePath("/path/to/file", fs.root)
+
+        #exitcode, *_ = fc("cat", "/file/does/not/exist")
+        #assert exitcode != 0
+
+        exitcode, *_ = fc("cat", testfile.path, input=contents)
+        assert exitcode == 0
+
+
+        exitcode, readback, _ = fc("cat", testfile.path)
+        assert exitcode == 0
+
+        #assert readback == contents

7296a60d16eb358e10d35e6732c35abc38e2d3ba
Hannes Weisbach <hannesweisbach@gmail.com>
2025-04-19 12:22:08 +0200

    wip: add rcopy unittest skeleton

--- tests/cmd/file_client.py
+++ tests/cmd/file_client.py
@@ -209,6 +209,21 @@ async def _unittest_tree(transport_factory: TransportFactory, fileserver_factory
         exitcode, readback, _ = fc("tree")
         assert exitcode == 0
 
+@pytest.mark.asyncio
+async def _unittest_rcopy(transport_factory: TransportFactory, fileserver_factory: FileServerFactory):
+    fs_node_id = 11
+    environment_variables = {
+        **transport_factory(100).environment,
+        "YAKUT_PATH": str(OUTPUT_DIR),
+    }
+
+    with fileserver_factory(fs_node_id, transport_factory) as fs:
+        fc = FileClient(fs_node_id, environment_variables)
+
+        # read / by default, if no path given
+        #exitcode, readback, _ = fc("rcopy", "src", "dst")
+        #assert exitcode == 0
+
 # TODO:
 # ls
 # rcpy
